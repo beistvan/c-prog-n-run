c57c1863ad19b61cd8ff08bc22521f8b63d91f29
Beregszászi István <beistvan@gmail.com>
2024-03-23 10:59:32 +0100

    Packaed Pointer For Addres Of The String In The Heap

---
+++ packaed-pointer-for-addres-of-the-string-in-the-heap.c
@@ -0,0 +1,29 @@
+struct heap_string {
+  char* addr;
+};
+
+size_t string_length(const char* s) {
+    size_t length = 0;
+    while(*s != 0) {
+        ++length;
+        ++s;
+    }
+    return length;
+}
+
+char* copy_string(const char* s) {
+    size_t length = string_length(s);
+    char* result = malloc(sizeof(char)*(length+1));
+    for (size_t i = 0; i <= length; ++i) {
+        result[i] = s[i];
+    }
+    return result;
+}
+
+struct heap_string halloc( const char* s ) {
+    return (struct heap_string) {.addr = copy_string(s)};
+}
+
+void heap_string_free( struct heap_string h ) {
+    free(h.addr);
+}
