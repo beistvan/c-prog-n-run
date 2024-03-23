2a74088bb272e7b5ee60ede6d0178a808c0d94f6
Beregszászi István <beistvan@gmail.com>
2024-03-23 11:55:10 +0100

    Union Ins Program Example

---
+++ uinion-ins-program-example.c
@@ -0,0 +1,19 @@
+const union ins program[] = {
+    { BC_IREAD },
+    { .as_arg64 = { BC_PUSH, .arg = 10 } },
+    { BC_ISUB },
+    { .as_arg64 = { BC_PUSH, .arg = 2 } },
+    { BC_IDIV },
+    { BC_IPRINT },
+    { BC_STOP }
+};
+
+/*
+ IREAD
+PUSH 10
+ISUB
+PUSH 2
+IDIV
+IPRINT
+STOP
+ */
