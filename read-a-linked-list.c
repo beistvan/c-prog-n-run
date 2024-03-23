b75c6c73a841800093f0d5fc361310c0db4e5f1a
Beregszászi István <beistvan@gmail.com>
2024-03-23 10:54:19 +0100

    Read A Linked List

---
+++ read-a-linked-list.c
@@ -0,0 +1,19 @@
+struct list* list_read() {   
+    struct list* start_node = NULL;
+    struct list* list = NULL;
+    struct maybe_int64 new_value = maybe_read_int64();
+    while (new_value.valid) {
+        struct list* new_node = node_create(new_value.value);
+        if (!list) {
+            list = new_node;
+            start_node = list;
+        }
+        else
+        {
+            list->next = new_node;
+            list = new_node;
+        }
+        new_value = maybe_read_int64();
+    }
+    return start_node;
+}
